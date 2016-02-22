84b73adb2bce708e85665a2a9e69d75c5c9f668e
Charles Luxton <charles.g.luxton@gmail.com>
2016-02-22 17:07:46 +1000

    Initial commit

--- SimpleSpotify/ViewController.swift
+++ SimpleSpotify/ViewController.swift
@@ -8,16 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SPTAuthViewDelegate, SPTAudioStreamingPlaybackDelegate {
 
+    let kClientID = "08860c10e16e4261a57a40469500a733"
+    let kCallbackURL = "simplespotify://returnafterlogin"
+    let kTokenSwapURL = "https://radiant-headland-43693.herokuapp.com/swap"
+    let kTokenRefreshURL = "https://radiant-headland-43693.herokuapp.com/refresh"
+    
+    var player: SPTAudioStreamingController?
+    let spotifyAuthenticator = SPTAuth.defaultInstance()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+                // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginWithSpotify(sender: AnyObject) {
+        spotifyAuthenticator.clientID = kClientID
+        spotifyAuthenticator.requestedScopes = [SPTAuthStreamingScope]
+        spotifyAuthenticator.redirectURL = NSURL(string: kCallbackURL)
+        spotifyAuthenticator.tokenSwapURL = NSURL(string: kTokenSwapURL)
+        spotifyAuthenticator.tokenRefreshURL = NSURL(string: kTokenRefreshURL)
+        
+        let spotifyAuthVC = SPTAuthViewController.authenticationViewController()
+        spotifyAuthVC.delegate = self
+        spotifyAuthVC.modalPresentationStyle = UIModalPresentationStyle.OverFullScreen
+        
+        spotifyAuthVC.definesPresentationContext = true
+        print("PRESENT")
+        presentViewController(spotifyAuthVC, animated: false, completion: nil)
+        
+    }
+    
+    // SPTAuthViewDelegate protocol methods
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didLoginWithSession session: SPTSession!) {
+        print("SETUP PLAYER")
+        setupSpotifyPlayer()
+        loginWithSpotifySession(session)
+    }
+    
+    func authenticationViewControllerDidCancelLogin(authenticationViewController: SPTAuthViewController!) {
+        print("login cancelled")
+    }
+    
+    func authenticationViewController(authenticationViewController: SPTAuthViewController!, didFailToLogin error: NSError!) {
+        print("login failed")
+    }
+    
+    // SPTAudioStreamingPlaybackDelegate protocol methods
+    
+    private
+    
+    func setupSpotifyPlayer() {
+        player = SPTAudioStreamingController(clientId: spotifyAuthenticator.clientID)
+        player!.playbackDelegate = self
+        player!.diskCache = SPTDiskCache(capacity: 1024 * 1024 * 64)
+    }
+    
+    func loginWithSpotifySession(session: SPTSession) {
+        player!.loginWithSession(session, callback: { (error: NSError!) in
+            if error != nil {
+                print("Couldn't login with session: \(error)")
+                return
+            }
+            self.useLoggedInPermissions()
+        })
+    }
+    
+    func useLoggedInPermissions() {
+        let spotifyURI = "spotify:track:1WJk986df8mpqpktoktlce"
+        player!.playURIs([NSURL(string: spotifyURI)!], withOptions: nil, callback: nil)
     }
 
 
